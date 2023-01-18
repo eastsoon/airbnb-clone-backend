6b78889caef1ff33d3ab937fedc9d7167d1d6ff9
EastSoon <jds920805@gmail.com>
2023-01-18 17:51:12 +0900

    11.19 Wishlists

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serialize import Wishlistserializer
+from .models import Wishlist
 
-# Create your views here.
+
+class Wishlists(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        all_wishlists = Wishlist.objects.filter(user=request.user)
+        serialize = Wishlistserializer(
+            all_wishlists,
+            many=True,
+            context={"request": request},
+        )
+
+        return Response(serialize.data)
+
+    def post(self, request):
+        serializer = Wishlistserializer(data=request.data)
+        if serializer.is_valid():
+            wishlist = serializer.save(
+                user=request.user,
+            )
+            serializer = Wishlistserializer(wishlist)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
