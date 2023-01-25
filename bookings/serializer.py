c58eca748cc2f5b60f09675b5c71506510311123
EastSoon <jds920805@gmail.com>
2023-01-25 16:12:53 +0900

    11.24 Validate Booking

--- bookings/serializer.py
+++ bookings/serializer.py
@@ -20,11 +20,24 @@ class CreateBookingSerializer(serializers.ModelSerializer):
         now = timezone.localtime(timezone.now()).date()
         if now > value:
             raise serializers.ValidationError("잘못된 날짜 입니다.")
+        return value
 
     def validate_check_out(self, value):
         now = timezone.localtime(timezone.now()).date()
         if now > value:
             raise serializers.ValidationError("잘못된 날짜 입니다.")
+        return value
+
+    def validate(self, data):
+        if data["check_in"] >= data["check_out"]:
+            raise serializers.ValidationError("체크아웃 날짜를 확인해주세요.")
+
+        if Booking.objects.filter(
+            check_in__lte=data["check_out"],
+            check_out__gte=data["check_in"],
+        ).exists():
+            raise serializers.ValidationError("이미 예약된 객실이 있습니다.")
+        return data
 
 
 class PublicBookingSerializer(serializers.ModelSerializer):
