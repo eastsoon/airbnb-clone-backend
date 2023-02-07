4b131baf6cdd01d5ee8ab8ecef58c3029532a4c4
EastSoon <jds920805@gmail.com>
2023-02-07 18:08:31 +0900

    14.7 Custom Resolvers

--- rooms/types.py
+++ rooms/types.py
@@ -1,7 +1,9 @@
 import strawberry
+import typing
 from strawberry import auto
 from . import models
 from users.types import UserType
+from reviews.types import ReviewType
 
 
 @strawberry.django.type(models.Room)
@@ -10,3 +12,14 @@ class RoomType:
     name: auto
     kind: auto
     owner: "UserType"
+
+    @strawberry.field
+    def reviews(self, page: int) -> typing.List["ReviewType"]:
+        page_size = 3
+        start = (page - 1) * page_size
+        end = start + page_size
+        return self.reviews.all()[start:end]
+
+    @strawberry.field
+    def rating(self) -> str:
+        return self.rating()
