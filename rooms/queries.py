2a365eff075cc5a57b48e2a7ac86408e88c0037b
EastSoon <jds920805@gmail.com>
2023-02-08 10:15:35 +0900

    ~14.9 Permissions

--- rooms/queries.py
+++ rooms/queries.py
@@ -1,8 +1,14 @@
+# from strawberry.types import Info
 from . import models
 
 
 def get_all_rooms():
+    # if info.context.request.user.is_authenticated:
     return models.Room.objects.all()
+
+
+# else:
+# raise Exception("Not aut.")
 
 
 def get_room(pk: int):
