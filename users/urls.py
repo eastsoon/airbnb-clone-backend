c0714986dfd88c7ca8ab6b675aa34181a72edaa9
EastSoon <jds920805@gmail.com>
2023-02-15 15:09:16 +0900

    15.4 JWT Encode

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("log-out", views.LogOut.as_view()),
     path("change-password", views.ChangePassword.as_view()),
     path("token-login", obtain_auth_token),
+    path("jwt-login", views.JWTLogIn.as_view()),
     path("@<str:username>", views.PublicUser.as_view()),
 ]
